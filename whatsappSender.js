67548402a3cb7a4b37677ac731206ddb8def18e2
felixor355 <40955727+felixor355@users.noreply.github.com>
2025-02-04 11:08:31 +1000

    Update whatsappSender.js

--- whatsappSender.js
+++ whatsappSender.js
@@ -1,1 +1,13 @@
+function shareTextViaWhatsApp() {
+    if (!validateForm() || !validateOrder(new FormData(document.getElementById('orderForm')))) return;
 
+    const form = document.getElementById('orderForm');
+    const formData = new FormData(form);
+    const orderData = generateOrderData(formData);
+
+    const whatsappMessage = encodeURIComponent(orderData);
+    const whatsappLink = `https://wa.me/?text=${whatsappMessage}`;
+    window.open(whatsappLink, '_blank');
+
+    clearFormData();
+}
